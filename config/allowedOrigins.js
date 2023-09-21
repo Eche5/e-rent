c775d78cf8136ca0580a25374648444d7eb2c683
Eche5 <111073744+Eche5@users.noreply.github.com>
2023-09-21 23:05:43 +0100

    updated cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://e-rent-eche5.vercel.app",
+  "https://e-rent-flwz1d7xt-eche5.vercel.app/",
   "http://localhost:5173",
   "http://127.0.0.1:5500",
   "http://localhost:3000",
