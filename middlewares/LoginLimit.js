8638061ece9e8376484096e815f613a27f88e22e
Eche5 <111073744+Eche5@users.noreply.github.com>
2023-09-21 22:44:39 +0100

    updated cors

--- middlewares/LoginLimit.js
+++ middlewares/LoginLimit.js
@@ -6,6 +6,5 @@ const limiter = rateLimit({
   limit: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  // store: ... , // Use an external store for more precise rate limiting
 });
 module.exports = limiter;
